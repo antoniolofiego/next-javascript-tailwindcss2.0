f9e377c6c0a35ba413e638032f99d7f0a3db7592
Antonio Lo Fiego <alofiego@hey.com>
2020-11-18 18:08:23 -0500

    Format fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,12 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>
-          Welcome to the home page!
-      </div>
-    </>
-  )
+      <>
+          <Head>
+              <title>Home Page</title>
+              <link rel='icon' href='/favicon.ico' />
+          </Head>
+          <div>Welcome to the home page!</div>
+      </>
+  );
 }
